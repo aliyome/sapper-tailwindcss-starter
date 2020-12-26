6e3ee66450ae14f057b1d284f1e66a0f06b72a05
aliyome <aliyome@gmail.com>
2020-12-27 01:39:39 +0900

    feat: Remove default styling

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,71 +3,52 @@
 </script>
 
 <style>
-  nav {
-    border-bottom: 1px solid rgba(255, 62, 0, 0.1);
-    font-weight: 300;
-    padding: 0 1em;
-  }
-
-  ul {
-    margin: 0;
-    padding: 0;
-  }
-
-  /* clearfix */
-  ul::after {
-    content: '';
-    display: block;
-    clear: both;
-  }
-
-  li {
-    display: block;
-    float: left;
-  }
-
-  [aria-current] {
+  ._bottom-line {
     position: relative;
-    display: inline-block;
   }
-
-  [aria-current]::after {
-    position: absolute;
+  ._bottom-line:before {
     content: '';
-    width: calc(100% - 1em);
-    height: 2px;
-    background-color: rgb(255, 62, 0);
     display: block;
-    bottom: -1px;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 0;
+    background: #0078f2;
+    transition: height 0.1s ease-in-out;
   }
-
-  a {
-    text-decoration: none;
-    padding: 1em 0.5em;
-    display: block;
+  ._bottom-line:hover:before {
+    height: 5px;
   }
 </style>
 
 <nav class="bg-green-600 px-4 text-white">
-  <ul>
-    <li>
-      <a
-        aria-current="{segment === undefined ? 'page' : undefined}"
-        href=".">home</a>
+  <ul class="flex">
+    <li class="_bottom-line">
+      <div class="p-4 ">
+        <a
+          class="hoge"
+          aria-current="{segment === undefined ? 'page' : undefined}"
+          href=".">home</a>
+      </div>
     </li>
-    <li>
-      <a
-        aria-current="{segment === 'about' ? 'page' : undefined}"
-        href="about">about</a>
+
+    <li class="_bottom-line">
+      <div class="p-4">
+        <a
+          aria-current="{segment === 'about' ? 'page' : undefined}"
+          href="about">about</a>
+      </div>
     </li>
 
     <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-    <li>
-      <a
-        rel="prefetch"
-        aria-current="{segment === 'blog' ? 'page' : undefined}"
-        href="blog">blog</a>
+    <li class="_bottom-line">
+      <div class="p-4">
+        <a
+          rel="prefetch"
+          aria-current="{segment === 'blog' ? 'page' : undefined}"
+          href="blog">blog</a>
+      </div>
     </li>
   </ul>
 </nav>
